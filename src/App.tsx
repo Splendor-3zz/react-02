d9a9312e91efec9299014bcb3bbbb6acc299fd62
Ezzaldeen <azooz151161@gmail.com>
2025-05-18 03:11:10 +0300

    error message toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/ui/Modal";
 import Button from "./components/ui/Button";
 import { formInputsList, productList } from "./data";
 import Input from "./components/ui/Input";
 import type { IProduct } from "./interfaces";
+import { productValidation } from "./validation";
+import ErrorMessage from "./components/ui/ErrorMessage";
 
-interface IProps {}
+const App = () => {
 
-const App = ({}: IProps) => {
-  const [product, setProduct] = useState <IProduct> ({
+  const defultProduct = {
     title: '',
     description: '',
     imageURL: '',
@@ -19,7 +20,9 @@ const App = ({}: IProps) => {
       name: '',
       imageURL: ''
     }
-  })
+  };
+  const [product, setProduct] = useState <IProduct> (defultProduct)
+  const [errors, setErrors] = useState ({title:"", description:"", imageURL:"", price:""});
   const [isOpen, setIsOpen] = useState(false);
 
   const open = () => {setIsOpen(true);}
@@ -31,13 +34,40 @@ const App = ({}: IProps) => {
       ...product,
       [name]: value,
     })
+
+    setErrors({
+      ...errors,
+      [name]: "",
+    })
+  }
+
+  const onCancel = () => {
+    setProduct(defultProduct);
+    close()
+  }
+
+ const supmitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const {title, description, price, imageURL} = product;
+    const errors = productValidation ({ title, description, price, imageURL})
+    console.log(errors)
+
+    const hasErrorMsg = Object.values(errors).some(value => value === "") &&  Object.values(errors).every(value => value === "");
+    console.log(hasErrorMsg)
+
+    if (!hasErrorMsg){
+      setErrors(errors);
+      return;
+    }
+
+    console.log("send this data")
   }
 
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
   const renderFormInputList = formInputsList.map((input) => (
-    <div className="flex flex-col">
+    <div className="flex flex-col" key={input.id}>
       <label
         htmlFor={input.id}
         className="mb-[2px] text-sm font-medium text-gray-700"
@@ -45,8 +75,10 @@ const App = ({}: IProps) => {
         {input.label}
       </label>
       <Input type="text" id={input.id} name={input.name} value={product[input.name]} onChange={onCangeHandeler} />
+      <ErrorMessage msg={errors[input.name]}/>
     </div>
   ));
+
   return (
     /* */
     <main className="container mx-auto">
@@ -57,10 +89,10 @@ const App = ({}: IProps) => {
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={close} title="Add a new product">
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={supmitHandler}>
           {renderFormInputList}
           <div className="flex items-center space-x-3">
-            <Button className=" bg-red-700" onClick={close}>
+            <Button className=" bg-red-700" onClick={onCancel}>
               Cancel
             </Button>
             <Button className=" bg-indigo-700">Submit</Button>
